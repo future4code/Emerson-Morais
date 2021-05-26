5eead522e370c817eca91e17656fb7fc904f691a
Emerson <emersonfm95@gmail.com>
2021-05-25 21:50:37 -0300

    operadores

---
+++ quinzena1/operadores/index.js
@@ -0,0 +1,74 @@
+ /* Exercício 01 
+false 
+false 
+true 
+boolean   */
+
+
+// Exercício 02 
+// A string não foi convertida em Number. Dessa forma só irá juntar os números
+// ao invés de somar
+
+
+//exercicio 03
+
+/* let primeiroNumero = Number (prompt("Digite um numero!"))
+let segundoNumero = Number (prompt("Digite outro numero!"))
+
+const soma = primeiroNumero + segundoNumero
+
+console.log(soma) - tem que converver a string em number e acrescentar
+os ) na equação.*/
+
+
+// exercício 01
+
+ /*let idade = Number(prompt("Qual a sua idade?"))
+ let melhorAmigo = Number(prompt("Qual a idade do seu melhor amigo?"))
+ console.log("Sua idade é maior do que a do seu melhor amigo?", idade > melhorAmigo)
+ console.log(idade - melhorAmigo) */
+
+//---------------------------------------------------------------------------------------------------
+
+// exercício 02
+
+/*let numeroPar = Number(prompt("Digite um número par."))
+let resto = numeroPar %2
+console.log(resto) */
+
+// todo número par o resultado será 0
+// todo número impar o resultado será 01
+
+//--------------------------------------------------------------------------------------------------
+
+// exercício 03
+
+/*let idadeUser = Number(prompt("digite sua idade."))
+const idadeMeses = idadeUser * 12
+const idadeDias = idadeUser * 365
+const idadeHoras = idadeUser * 8760
+console.log("Idade em meses:", idadeMeses)
+console.log("Idade em dias", idadeDias)
+console.log("Idade em horas", idadeHoras)*/
+
+
+/*Resultado: idade em meses 300
+             idade em dias: 9125
+             idade em horas: 219000*/
+
+
+
+// Exercício 04
+
+/* let numero1= Number(prompt("Digite um número."))
+let numero2 = Number(prompt("digite outro número"))
+console.log("O primeiro número é maior que o segundo?", (numero1 > numero2))
+console.log("O primeiro numero é iguala ao segundo?", (numero1 == numero2))
+console.log("O primeiro numero é divisivel pelo segundo?", (numero1 % numero2) ==0)
+console.log("O segundo numero é divisível pelo primeiro?", (numero2 / numero1) ==0) */
+
+// Resposta: false para todas, porém depende do número inserido.
+
+
+
+
