8f4cb653369ffe6dfb5abdf1e0168c2bc5ab6788
Emerson <emersonfm95@gmail.com>
2021-06-01 22:23:13 -0300

    exercícios de funções

---
+++ quinzena1/funcoes/index.js
@@ -0,0 +1,103 @@
+//Resposta dos exercícios 31.05
+
+// exercício de interpretação 01
+// a) 10 e 50
+// b) não aconteceria nada.
+
+// exercício de interpretação 02
+
+// a) determina que os caracteres fiquem em minúsculo
+// e compara as string informando se são true ou false.
+
+// b) true para todas.
+
+//---------------------------------------------------------------
+
+// exercício de escrita de código
+
+//01
+ // a) 
+     //function imprimirMeusDados() {
+      //  console.log("Eu sou Emerson, tenho 25 anos, moro em São Paulo e sou estudante.")
+    //}
+
+    //imprimirMeusDados()
+
+    // b)
+
+         /*   function meusDados (nome, idade, cidade, profissão) {
+        const dadosUsuario = `Eu sou ${nome}, tenho ${idade} anos, moro em ${cidade} e sou ${profissão}.`
+        console.log(dadosUsuario) 
+       }
+       let nome = "Emerson"
+       let idade = 25
+       let cidade = "São Paulo"
+       let cargo = "Estudante"
+   
+       meusDados(nome, idade,cidade,profissão) */
+
+   
+   //-----------------------------------------------------------------------------------------
+
+   // 02
+         //a)
+            /* function somarArea(cinco, cinco) {
+                const area = 5 + 5
+                console.log(area)
+            }
+            somarArea(5, 5)
+
+        // b)
+        function somarNumeros(numero1, numero2) {
+            const soma = 4 <= 5
+            console.log(soma)
+        }
+
+        somarNumeros()   */
+        
+        // c) Não consegui fazer
+
+       
+
+
+        // d) 
+        
+       /* function verificarTamanhoPalavraImprimirCapsLoock(palavra) {
+    console.log("O total de palavras: " + palavra.lenght + " frase em capsloock: " + palavra.toUpperCase())
+}
+const palavra = prompt("Digite alguma coisa")
+verificarTamanhoPalavraImprimirCapsLoock(palavra) */
+
+
+// exercicio 03
+
+/* const número1 = Number(prompt("Insira um número:"))
+const número2 = Number(prompt("Insira outro número:"))
+
+function somar(número1, número2) {
+    const soma = número1 + número2
+    return soma
+}
+
+function subtrair (número1, número2) {
+const subtração = número1 - número2
+return subtração
+}
+
+function multiplicar(número1, número2) {
+    const multiplicação = número1 * número2
+    return multiplicação
+
+}
+
+function dividir(número1, número2) {
+    const divisão = número1 / número2
+    return divisão
+
+}
+
+console.log("Números inseridos:", número1 + " e " + número2)
+console.log("Soma:", somar (número1, número2))
+console.log("Subtração:", subtrair (número1, número2))
+console.log("Multiplicação:", multiplicar (número1, número2))
+console.log("Divisão:", dividir(número1, número2)) */
